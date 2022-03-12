136e278dd6d30c5ae16472e473aa08d1bfe92267
pract <hart10@tut.by>
2022-03-12 19:01:48 +0300

    Add1

---
+++ ta_training-java/src/main/java/com/epam/training/Andrej_Paulau/fundamental/main_task/HelloName.java
@@ -0,0 +1,9 @@
+package com.epam.training.Andrej_Paulau.fundamental.main_task;
+
+import java.util.Arrays;
+
+public class HelloName {
+    public static void main(String[] args) {
+        System.out.println("Hello" + Arrays.toString(args));
+    }
+}
