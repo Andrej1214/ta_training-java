c4a308f4ffbe8936d70cde1dd4d53b9a916b064c
pract <hart10@tut.by>
2022-03-13 02:16:36 +0300

    Add2

--- ta_training-java/src/main/java/com/epam/training/Andrej_Paulau/fundamental/main_task/HelloName.java
+++ ta_training-java/src/main/java/com/epam/training/Andrej_Paulau/fundamental/main_task/HelloName.java
@@ -1,9 +1,15 @@
 package com.epam.training.Andrej_Paulau.fundamental.main_task;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HelloName {
     public static void main(String[] args) {
-        System.out.println("Hello" + Arrays.toString(args));
+        String str = "Hello, " + Arrays.toString(args) + "!";
+        Pattern pattern = Pattern.compile("(\\[|\\])");
+        Matcher matcher = pattern.matcher(str);
+        String str1 = matcher.replaceAll("");
+        System.out.println(str1);
     }
 }
